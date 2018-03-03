56e769ff1733a0744645fc0f62eefdb79ceb93be
Mattia Ciollaro <mattiaciollaro@gmail.com>
2018-03-03 15:12:09 -0500

    Add minor changes

--- client/r/r_lintr.sh
+++ client/r/r_lintr.sh
@@ -3,12 +3,12 @@
 # This is a modified version of https://gist.github.com/wookietreiber/afdb946625c6090f96012ee1da316a73#file-git-hook-lintr-r
 #
 if git rev-parse --verify HEAD &> /dev/null ; then
-    against = HEAD
+    against=HEAD
 else
-    against = 4b825dc642cb6eb9a060e54bf8d69288fbee4904
+    against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
 fi
 
-files = $(git diff-index --cached --name-only $against | grep -E '\.[rR]$')
+files=$(git diff-index --cached --name-only $against | grep -E '\.[rR]$')
 
 if [[ -n $files ]] ; then
     if ! command -v Rscript &> /dev/null ; then
@@ -20,13 +20,6 @@ if [[ -n $files ]] ; then
         echo "Commit aborted: please install the lintr package in R." >&2
         exit 1
     fi
-
-    if ! Rscript --vanilla -e 'library(littler)' &> /dev/null ; then
-        echo "Commit aborted: please install the littler package in R." >&2
-        exit 1
-    fi
 fi
 
-for file in $files ; do
-    Rscript --vanilla .git/hooks/git-hook-lintr.R $file || exit 1
-done
+Rscript --vanilla .git/hooks/r_lintr.R $files || exit 1
--- client/r/r_lintr_pre_commit.sh
+++ client/r/r_lintr_pre_commit.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-# install required R dependencies
-R CMD -e "install.packages(c('lintr', 'littlr'), repos = 'https://cloud.r-project.org'"
+# get path to target repository
+repo=$1
+
+# install lintr, if not already installed
+if ! Rscript --vanilla -e "library(lintr)" &> /dev/null ; then
+    Rscript --vanilla -e "install.packages('lintr', repos = 'https://cloud.r-project.org')"
+fi
 
 # add the R lintr utility script
-cp r_lintr.R ./.git/hooks/
+cp ./client/r/r_lintr.R $repo/.git/hooks/
 
 # add the hook as .git/hooks/pre-commit
-cp r_lintr.sh ./.git/hooks/pre-commit
+cp ./client/r/r_lintr.sh $repo/.git/hooks/pre-commit
+chmod +x $repo/.git/hooks/pre-commit
