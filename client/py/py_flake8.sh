b24d4d18a81a4ba20b172d3c0cdc1a99ffcf8b12
mattiaciollaro <mattiaciollaro@gmail.com>
2018-03-07 13:35:28 -0500

    Add a shell git diff-index for python

---
+++ client/py/py_flake8.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+if git rev-parse --verify HEAD &> /dev/null; then
+    AGAINST=HEAD
+else
+    AGAINST=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+fi
+
+files=$(git diff-index --cached --name-only $AGAINST | grep -E '\.py$')
+
+if [[ -n $files ]]; then
+    python .git/hooks/py_flake8.py $files
+fi
