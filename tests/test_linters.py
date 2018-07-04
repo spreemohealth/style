c222aefb73f871c37dd6a94126b403da5f4f9c07
Mattia Ciollaro <mattiaciollaro@gmail.com>
2018-07-04 11:09:33 -0400

    Add linters config tests in test_linters.py

--- tests/test_linters.py
+++ tests/test_linters.py
@@ -71,6 +71,40 @@ class TestMarkdownLinter(TestCase):
             # clean up
             w.delete()
 
+    def test_linter_config_file(self):
+        try:
+            # write the `markdownlint.json` config file instructing to
+            # except "MD022/blanks-around-headers"
+            config_file_path = path.join(self.cwd, ".markdownlint.json")
+            w_conf = Writer(config_file_path)
+            w_conf.write("{")
+            w_conf.write('    "MD022": false')
+            w_conf.write("}")
+
+            # write a bad markdown file
+            file_path = path.join(self.cwd, "test.md")
+            w = Writer(file_path)
+            w.write("# Section")
+            w.write("Missing a blank line after header.")
+
+            # instantiate the linter (sink STDOUT in testing)
+            f = StringIO()
+            with redirect_stdout(f):
+                return_value = (
+                    MarkdownLinter(config_path=w_conf.path).lint([w.path])
+                )
+
+            # return value will be 0 if MD022 is correctly ignored
+            self.assertEqual(return_value, 0)
+
+        except Exception:
+            raise
+
+        finally:
+            # clean up
+            w_conf.delete()
+            w.delete()
+
 
 class TestPythonLinter(TestCase):
 
@@ -78,7 +112,7 @@ class TestPythonLinter(TestCase):
 
     def test_zero_exit_code(self):
         try:
-            # write a bad markdown file
+            # write a bad python file
             file_path = path.join(self.cwd, "test.py")
             w = Writer(file_path)
             w.write("# This is ok")
@@ -108,7 +142,7 @@ class TestPythonLinter(TestCase):
             w.write("# No space around =")
             w.write("foo=1")
 
-            # instantiate the linter
+            # instantiate the linter (sink STDOUT in testing)
             f = StringIO()
             with redirect_stdout(f):
                 return_value = PythonLinter().lint([w.path])
@@ -122,6 +156,39 @@ class TestPythonLinter(TestCase):
             # clean up
             w.delete()
 
+    def test_linter_config_file(self):
+        try:
+            # write the `.flake8` config file instructing to
+            # except "E225"
+            config_file_path = path.join(self.cwd, ".flake8")
+            w_conf = Writer(config_file_path)
+            w_conf.write("[flake8]")
+            w_conf.write("ignore = E225")
+
+            # write a bad python file
+            file_path = path.join(self.cwd, "test.py")
+            w = Writer(file_path)
+            w.write("# No space around =")
+            w.write("foo=1")
+
+            # instantiate the linter (sink STDOUT in testing)
+            f = StringIO()
+            with redirect_stdout(f):
+                return_value = (
+                    PythonLinter(config_path=w_conf.path).lint([w.path])
+                )
+
+            # return value will be 0 if E225 is correctly ignored
+            self.assertEqual(return_value, 0)
+
+        except Exception:
+            raise
+
+        finally:
+            # clean up
+            w_conf.delete()
+            w.delete()
+
 
 class TestRLinter(TestCase):
 
@@ -129,7 +196,7 @@ class TestRLinter(TestCase):
 
     def test_zero_exit_code(self):
         try:
-            # write a bad markdown file
+            # write a bad R file
             file_path = path.join(self.cwd, "test.R")
             w = Writer(file_path)
             w.write("# This is ok")
@@ -153,13 +220,13 @@ class TestRLinter(TestCase):
 
     def test_non_zero_exit_code(self):
         try:
-            # write a bad python file
+            # write a bad R file
             file_path = path.join(self.cwd, "test.R")
             w = Writer(file_path)
             w.write("# Use = instead of <-")
             w.write("bar = 0")
 
-            # instantiate the linter
+            # instantiate the linter (sink STDOUT in testing)
             f = StringIO()
             with redirect_stdout(f):
                 return_value = RLinter().lint([w.path])
@@ -171,6 +238,39 @@ class TestRLinter(TestCase):
 
         finally:
             # clean up
+            w.delete()
+
+    def test_linter_config_file(self):
+        try:
+            # write the `.lintr` config file instructing to
+            # deactivate the "assignment_linter"
+            config_file_path = path.join(self.cwd, ".lintr")
+            w_conf = Writer(config_file_path)
+            w_conf.write("linters: with_defaults(assignment_linter = NULL)")
+
+            # write a bad R file
+            file_path = path.join(self.cwd, "test.R")
+            w = Writer(file_path)
+            w.write("# Use = instead of <-")
+            w.write("bar = 0")
+
+            # instantiate the linter
+            f = StringIO()
+            with redirect_stdout(f):
+                return_value = (
+                    RLinter(config_path=w_conf.path).lint([w.path])
+                )
+
+            # return value will be 0 if the "assignment_linter" is correctly
+            # deactivated
+            self.assertEqual(return_value, 0)
+
+        except Exception:
+            raise
+
+        finally:
+            # clean up
+            w_conf.delete()
             w.delete()
 
 
